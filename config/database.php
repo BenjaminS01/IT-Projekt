6f9fe4c3f9e208cb1d08bbc83aae80b12f2dfa8d
BenjaminS01 <bswarovsky@gmx.net>
2020-08-24 12:47:38 +0200

    herokuDB

--- config/database.php
+++ config/database.php
@@ -1,9 +1,9 @@
 <?php
-$dbName='trainingskalender';
-$dns='mysql:dbname='.$dbName.';host=localhost;';
+$dbName='heroku_baf632cb15a3418';
+$dns='mysql:dbname='.$dbName.';host=eu-cdbr-west-03.cleardb.net;';
 
-$userName='root';
-$password='';
+$userName='b6eb0684d0bb0a';
+$password='f80e7fb6';
 $options=[
     PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
     PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
