a4ab22d19ddef5946ed97e07045db923391eac35
BenjaminS01 <bswarovsky@gmx.net>
2020-09-01 23:50:41 +0200

    sql skripte

---
+++ database/Timeslot(U1.1).sql
@@ -0,0 +1,16 @@
+/*
+-- Query: 
+-- Date: 2020-09-01 22:55
+*/
+INSERT INTO `` (`id`,`startTime`,`endTime`) VALUES (NULL,'08:35:00','08:40:00');
+INSERT INTO `` (`id`,`startTime`,`endTime`) VALUES (NULL,'09:35:00','09:40:00');
+INSERT INTO `` (`id`,`startTime`,`endTime`) VALUES (NULL,'10:35:00','10:40:00');
+INSERT INTO `` (`id`,`startTime`,`endTime`) VALUES (NULL,'11:35:00','11:40:00');
+INSERT INTO `` (`id`,`startTime`,`endTime`) VALUES (NULL,'12:35:00','12:40:00');
+INSERT INTO `` (`id`,`startTime`,`endTime`) VALUES (NULL,'13:35:00','13:40:00');
+INSERT INTO `` (`id`,`startTime`,`endTime`) VALUES (NULL,'14:35:00','14:40:00');
+INSERT INTO `` (`id`,`startTime`,`endTime`) VALUES (NULL,'15:35:00','15:40:00');
+INSERT INTO `` (`id`,`startTime`,`endTime`) VALUES (NULL,'16:35:00','16:40:00');
+INSERT INTO `` (`id`,`startTime`,`endTime`) VALUES (NULL,'17:35:00','17:40:00');
+INSERT INTO `` (`id`,`startTime`,`endTime`) VALUES (NULL,'18:35:00','18:40:00');
+INSERT INTO `` (`id`,`startTime`,`endTime`) VALUES (NULL,'19:35:00','19:40:00');
