e3b98bae28de0b3f69f34ac03a9f44dcf138b43e
BenjaminS01 <bswarovsky@gmx.net>
2020-09-15 01:58:13 +0200

    2.0 3

--- config/imports.php
+++ config/imports.php
@@ -1,13 +1,16 @@
 <?php
 
-require_once 'core/controller.class.php';
-require_once 'helper/functions.php';
 require_once 'models/baseModel.class.php';
+require_once 'config/database.php';
+require_once 'config/init.php';
+
+
 require_once 'models/member.class.php';
 require_once 'models/trainingEntry.class.php';
 require_once 'models/timeslot.class.php';
 require_once 'models/viewAreaTimeslot.class.php';
 require_once 'models/area.class.php';
 require_once 'models/areaTimeslot.class.php';
-require_once 'config/database.php';
 require_once 'models/times.php';
+require_once 'core/controller.class.php';
+require_once 'helper/functions.php';
